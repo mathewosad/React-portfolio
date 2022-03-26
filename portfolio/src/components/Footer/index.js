75928a795fef2f05d2ffe3931708dbb5ee4b40c7
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-03-25 23:00:52 -0700

    footer.js

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -8,9 +8,9 @@ function Footer() {
     return (
         <footer className="container">
             <div>
-                <h3>Let's connect</h3>
-                <a href="https://github.com/d-belleza" target="blank"><img alt="github icon" src={Github}></img></a>
-                <a href="https://linkedin.com/in/david-belleza" target="blank"><img alt="linkedin icon" src={LinkedIn}></img></a>
+                <h3>Let's Link</h3>
+                <a href="https://github.com/mathewosad" target="blank"><img alt="github icon" src={Github}></img></a>
+                <a href="https://www.linkedin.com/in/mathew-osadolor-848b33177/" target="blank"><img alt="linkedin icon" src={LinkedIn}></img></a>
             </div>
         </footer>
     );
